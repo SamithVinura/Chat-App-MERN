1d11383c8a83a92bd17a63b0c4c22bb6b80aa96f
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-07-20 07:42:59 +0530

    create header using mat toolbar

--- frontend/src/components/authentication/Login.js
+++ frontend/src/components/authentication/Login.js
@@ -1,14 +1,77 @@
 import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
 import React, { useState } from 'react'
+import { useToast } from "@chakra-ui/toast";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
+    const toast = useToast();
+    const history = useHistory()
+
     const [show, setShow] = useState(false);
     const handleClick = () => setShow(!show);
 
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
+    const [loading,setLoading] = useState()
 
-    const submitHandler = () => {
+    const submitHandler = async() => {
+          setLoading(true)
+
+        if (!email || !password ) {
+            toast({
+                title: "Please Fill all the Feilds",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+            return;
+        }
+
+       
+
+        try {
+
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                },
+            }
+
+            const { data } = await axios.post(
+                "/api/user/login",
+                {
+                    
+                    email,
+                    password,
+                    
+                },
+                config
+            );
+            toast({
+                title: "Login Successful",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            localStorage.setItem("userInfo", JSON.stringify(data))
+            setLoading(false)
+            history.push("chats")
+        } catch (error) {
+            toast({
+                title: "Error Occured!",
+                description: error.response.data.message,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+
+        }
     }
     return (
         <VStack spacing="5px">
@@ -17,6 +80,7 @@ const Login = () => {
                 <FormLabel>Email</FormLabel>
                 <Input
                     placeholder="Enter Your Email"
+                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
             </FormControl>
@@ -26,6 +90,7 @@ const Login = () => {
                     <Input
                         type={show ? "text" : "password"}
                         placeholder="Password"
+                        value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
                     <InputRightElement width="4.5rem">
@@ -54,6 +119,7 @@ const Login = () => {
                     setEmail("guest@example.com");
                     setPassword("123456");
                 }}
+                isLoading={loading}
             >
                 Get Guest User Credentials
             </Button>
